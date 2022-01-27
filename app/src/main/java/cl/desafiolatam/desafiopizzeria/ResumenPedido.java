036c89f04041a70ac30affda24786ddd1e681254
Andrea Boccardo <andrea.boccardo@gmail.com>
2022-01-27 11:56:57 -0300

    Se agrega paleta de colores y tema con que se trabajará

---
+++ app/src/main/java/cl/desafiolatam/desafiopizzeria/ResumenPedido.java
@@ -0,0 +1,17 @@
+package cl.desafiolatam.desafiopizzeria;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ResumenPedido extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+
+    }
+}
